10202aaea3b319644aa752132ef7dfff001e138e
Dan Bryant <dan@perfectcu.be>
2016-10-27 10:37:02 -0700

    migrate to Flash component (CakePHP 2.9.x)

--- Controller/BoostCakeController.php
+++ Controller/BoostCakeController.php
@@ -40,14 +40,14 @@ class BoostCakeController extends AppController {
  * @return void
  */
 	public function bootstrap2() {
-		$this->Session->setFlash(__('Alert notice message testing...'), 'alert', array(
+		$this->Flash->set(__('Alert notice message testing...'), 'alert', array(
 			'plugin' => 'BoostCake',
 		), 'notice');
-		$this->Session->setFlash(__('Alert success message testing...'), 'alert', array(
+		$this->Flash->set(__('Alert success message testing...'), 'alert', array(
 			'plugin' => 'BoostCake',
 			'class' => 'alert-success'
 		), 'success');
-		$this->Session->setFlash(__('Alert error message testing...'), 'alert', array(
+		$this->Flash->set(__('Alert error message testing...'), 'alert', array(
 			'plugin' => 'BoostCake',
 			'class' => 'alert-error'
 		), 'error');
@@ -59,19 +59,19 @@ class BoostCakeController extends AppController {
  * @return void
  */
 	public function bootstrap3() {
-		$this->Session->setFlash(__('Alert success message testing...'), 'alert', array(
+		$this->Flash->set(__('Alert success message testing...'), 'alert', array(
 			'plugin' => 'BoostCake',
 			'class' => 'alert-success'
 		), 'success');
-		$this->Session->setFlash(__('Alert info message testing...'), 'alert', array(
+		$this->Flash->set(__('Alert info message testing...'), 'alert', array(
 			'plugin' => 'BoostCake',
 			'class' => 'alert-info'
 		), 'info');
-		$this->Session->setFlash(__('Alert warning message testing...'), 'alert', array(
+		$this->Flash->set(__('Alert warning message testing...'), 'alert', array(
 			'plugin' => 'BoostCake',
 			'class' => 'alert-warning'
 		), 'warning');
-		$this->Session->setFlash(__('Alert danger message testing...'), 'alert', array(
+		$this->Flash->set(__('Alert danger message testing...'), 'alert', array(
 			'plugin' => 'BoostCake',
 			'class' => 'alert-danger'
 		), 'danger');
--- View/Elements/bootstrap3/alerts.ctp
+++ View/Elements/bootstrap3/alerts.ctp
@@ -3,22 +3,22 @@
 echo $this->Session->flash();
 
 // Controller
-$this->Session->setFlash(__('Alert success message testing...'), 'alert', array(
+$this->Flash->set(__('Alert success message testing...'), 'alert', array(
 	'plugin' => 'BoostCake',
 	'class' => 'alert-success'
 ));
 
-$this->Session->setFlash(__('Alert info message testing...'), 'alert', array(
+$this->Flash->set(__('Alert info message testing...'), 'alert', array(
 	'plugin' => 'BoostCake',
 	'class' => 'alert-info'
 ));
 
-$this->Session->setFlash(__('Alert warning message testing...'), 'alert', array(
+$this->Flash->set(__('Alert warning message testing...'), 'alert', array(
 	'plugin' => 'BoostCake',
 	'class' => 'alert-warning'
 ));
 
-$this->Session->setFlash(__('Alert danger message testing...'), 'alert', array(
+$this->Flash->set(__('Alert danger message testing...'), 'alert', array(
 	'plugin' => 'BoostCake',
 	'class' => 'alert-danger'
 ));
